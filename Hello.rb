1d9935484f2f9e979f125e80384c2438a73ad9e0
NotNewAnymore <Henryfleischer01@gmail.com>
2024-01-13 22:55:54 -0800

    Updated Hello.rb

--- Hello.rb
+++ Hello.rb
@@ -1,2 +1,3 @@
 puts "Hello Mr. Cram!"
 puts "This assignment feels very AI generated, I was told to edit this document without having created it or gotten it."
+puts "This term, I want to learn how to write more effective algorithms, so that when I work on my FPS project, I can optimize it to a reasonable degree"
